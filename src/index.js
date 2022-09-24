0b951572e4f31e69e5303de93305ba4cd972b37a
Dima1409 <57713733+Dima1409@users.noreply.github.com>
2022-09-24 08:41:09 +0300

    btn scloll top

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import getRefs from './js/get-refs';
 import NotifyAlert from './js/notify';
 import LoadBtnMore from './js/btn-load-more';
 import AxiosService from './js/api-service';
+import scrollBtn from './js/btn-top';
 import { createCardsImage, clearCardsImage } from './js/createCards';
 
 const refs = getRefs();
 const notify = new NotifyAlert();
 const api = new AxiosService();
 const btnMore = new LoadBtnMore();
+scrollBtn();
 
 refs.searchForm.addEventListener('submit', onSubmitForm);
 refs.loadMore.addEventListener('click', onClickBtnLoadMore);
@@ -22,10 +24,8 @@ async function onSubmitForm(event) {
   api.resetPage();
 
   console.log(api.query);
-  
 
   try {
-    
     const data = await api.fetchCards();
     if (api.query === '') {
       return notify.onSearchNull();
@@ -33,13 +33,13 @@ async function onSubmitForm(event) {
     if (!api.query || data.data.totalHits === 0) {
       return notify.onError();
     }
-   
+
     notify.onSuccess(data.data.totalHits);
     createCardsImage(data.data);
-    console.log(data.data.totalHits)
-    console.log(api.perPage)
-    if(api.perPage>data.data.totalHits) {
-     return btnMore.hideBtnLoadMore()
+    console.log(data.data.totalHits);
+    console.log(api.perPage);
+    if (api.perPage > data.data.totalHits) {
+      return btnMore.hideBtnLoadMore();
     }
     btnMore.showBtnLoadMore();
     btnMore.enableBtn();
@@ -63,31 +63,3 @@ async function onClickBtnLoadMore() {
     console.warn(error);
   }
 }
-
-// const { height: cardHeight } = document
-//   .querySelector('.gallery')
-//   .firstElementChild.getBoundingClientRect();
-
-// window.scrollBy({
-//   top: cardHeight * 2,
-//   behavior: 'smooth',
-// });
-
-
-refs.btnTop.addEventListener('click', topFunction);
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    refs.btnTop.style.display = "block";
-  } else {
-    refs.btnTop.style.display = "none";
-  }
-}
-
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  }
-
--- src/js/btn-top.js
+++ src/js/btn-top.js
@@ -2,20 +2,40 @@ import getRefs from './get-refs';
 
 const refs = getRefs();
 
-// refs.btnTop.addEventListener('click', topFunction);
+export default function scrollBtn() {
+  refs.btnTop.addEventListener('click', up);
+  //   refs.btnTop.addEventListener('click', topFunction);
 
-// window.onscroll = function() {scrollFunction()};
+  window.onscroll = function () {
+    scrollFunction();
+  };
 
-// function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     refs.btnTop.style.display = "block";
-//   } else {
-//     refs.btnTop.style.display = "none";
-//   }
-// }
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 200 ||
+      document.documentElement.scrollTop > 200
+    ) {
+      refs.btnTop.style.display = 'flex';
+    } else {
+      refs.btnTop.style.display = 'none';
+    }
+  }
 
-// function topFunction() {
-//     document.body.scrollTop = 0;
-//     document.documentElement.scrollTop = 0;
-//   }
+  //   function topFunction() {
+  //     document.body.scrollTop = 0;
+  //     document.documentElement.scrollTop = 0;
+  //   }
 
+  let t;
+  function up() {
+    const top = Math.max(
+      document.body.scrollTop,
+      document.documentElement.scrollTop
+    );
+    if (top > 0) {
+      window.scrollBy(0, -100);
+      t = setTimeout(up, 20);
+    } else clearTimeout(t);
+    return false;
+  }
+}
